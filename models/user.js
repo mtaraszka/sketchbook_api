1ef398dd85ed6d8673ef2c14e27c13a43ff40ae1
Mateusz Taraszka <m.taraszka98@gmail.com>
2022-07-28 18:53:50 +0200

    create user schema

---
+++ models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema =  new Schema({
+    name: {
+        type: String,
+        minLenth: 5,
+        maxLength: 25,
+        required: [true, 'Name not provided.']
+    },
+    email: {
+        type: String,
+        unique: [true, 'Email already taken.'],
+        lowercase: true, 
+        trim: true,
+        required: [true, 'Email not provided.'],
+        validate: {
+            validator: (value) => {
+                return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+            },
+            message: `${value} is not a valid email!`
+        }
+    },
+    password: {
+        type: String,
+        minLenth: 10,
+        maxLength: 25,
+        required: [true, 'Password not provided.']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+})
+
+module.exports = mongoose.model('User', userSchema);
