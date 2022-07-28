2debbb8cafa613aa3bd03849985e12ef9091292a
Mateusz Taraszka <m.taraszka98@gmail.com>
2022-07-28 18:21:28 +0200

    add dotenv module, connection to database

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.db, { 
+    useUnifiedTopology: true,
+    useNewUrlParser: true 
+});
+
+const database = mongoose.connection;
+
+database.on('error', console.error.bind(console, 'connection error:'));
+database.once('open', () => {
+  console.log('connected with database');
+})
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
